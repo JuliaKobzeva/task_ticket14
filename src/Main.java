124f3c0f18c0cf7dedfc7958fe1a2cf0337e3a99
Юля Кобзева <selemina@mail.ru>
2023-03-09 16:28:08 +0300

    коммит1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.function.UnaryOperator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
