cfc3e33a5471e91d0ef92b3bb44c8c892b08f554
Юля Кобзева <selemina@mail.ru>
2023-03-09 16:30:05 +0300

    задание

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,6 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] array = {1,2,3,4,5,6,7,8,9,10};
-
-        for (int i = 0; i < array.length; i++) {
-            if(array[i]%2==0 ){
-                array[i] = array[i]+1;
-            }
-        }
-
-        System.out.println(Arrays.toString(array));
-
-        Arrays.stream(array)
-                .skip(2)
-                .limit(5)
-                .forEach(e->System.out.print(e + " "));
-
 
     }
 }
